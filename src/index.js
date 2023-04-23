db01264fd012f606ead1926a323764fd500ac501
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-23 06:55:39 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { ApiFalleryImg } from './fetchFalleryImg.js';
+import { typeFailure, typeSuccess, typeInfo } from './message.js';
 
+const apiService = new ApiFalleryImg();
+
+const searchForm = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const btnFindMore = document.querySelector('.load-more');
