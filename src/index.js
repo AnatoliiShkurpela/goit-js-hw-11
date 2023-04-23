c7189cdd01746b20cb27d451fd1189f32dd84f8a
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-23 07:02:56 +0300

    new

--- src/index.js
+++ src/index.js
@@ -61,3 +61,43 @@ function onFindMore() {
 function appendMarkup(hits) {
     gallery.insertAdjacentHTML('beforeend', createDataMarkup(hits));
   }
+  
+function createDataMarkup(hits) {
+    return hits
+      .map(
+        ({
+          largeImageURL,
+          webformatURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `
+    
+    <div class="gallery__item">
+    <a class="gallery__link" href="${largeImageURL}">
+    <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+  </div>
+  </a>
+  </div>
+  `;
+        }
+      )
+      .join(' ');
+  }
+  
