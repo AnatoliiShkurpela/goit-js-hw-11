edfe31e1a2ede4cbf845c034b1bb5fe6c2422b02
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-23 06:56:52 +0300

    new

--- src/index.js
+++ src/index.js
@@ -9,3 +9,28 @@ const apiService = new ApiFalleryImg();
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const btnFindMore = document.querySelector('.load-more');
+
+searchForm.addEventListener('submit', onSearch);
+btnFindMore.addEventListener('click', onFindMore);
+
+function onSearch(event) {
+  event.preventDefault();
+
+  apiService.query = event.target.elements.searchQuery.value.trim();
+  hiddBtn();
+  apiService.resetPage();
+  apiService
+    .fetchFalleryImg()
+    .then(({ hits, totalHits }) => {
+      if (hits.length === 0) {
+        showError();
+      } else {
+        clearMarkup();
+        appendMarkup(hits);
+        createGallery();
+        showMessage(typeSuccess, `Hooray! We found ${totalHits} images.`);
+        showBtn();
+      }
+    })
+    .catch(showError);
+}
